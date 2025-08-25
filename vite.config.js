6a4692de6fbdcfafcea36bf0fe79a919b3f3562a
Yahor Zubkou <taurusdoge@gmail.com>
2025-08-25 12:47:26 +0200

    FEATURE: add js minification, seo tags, improve build

--- vite.config.js
+++ vite.config.js
@@ -15,10 +15,26 @@ export default defineConfig({
     })
   ],
   build: {
+    minify: 'terser',
+    cssMinify: true,
+    target: 'es2020',
     rollupOptions: {
       output: {
-        manualChunks: undefined,
-      }
+        manualChunks: {
+          vendor: ['vite']
+        },
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      },
+      treeshake: true
+    },
+    reportCompressedSize: false,
+    chunkSizeWarningLimit: 1000
+  },
+  server: {
+    headers: {
+      'Cache-Control': 'public, max-age=31536000, immutable'
     }
   }
 })
